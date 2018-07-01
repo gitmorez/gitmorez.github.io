293802f9ba939c6c1e88659bd4c9596b4ca31506
gitmorez <40150179+gitmorez@users.noreply.github.com>
2018-07-02 01:56:08 +0300

    Add files via upload

--- carusel.js
+++ carusel.js
@@ -1,49 +1,58 @@
 window.onload = function() {
-	load();
+	load(0);
+	load(1);
+	load(2);
 }
 
-var prevbtn 	 = document.querySelector(".next");
-var nextbtn 	 = document.querySelector(".prev");
-var carusel  	 = document.querySelector(".carusel-block");
-var position 	 = 0;
+var prevbtn 	 = document.querySelectorAll(".next");
+var nextbtn 	 = document.querySelectorAll(".prev");
+var carusel  	 = document.querySelectorAll(".carusel-block");
+var position 	 = [0, 0, 0];
 
-function next() {
-	if (position <= 2023 && document.documentElement.clientWidth > 1300) {
-		position += 289; 
-		carusel.style.right = position + 'px';
+function next(el) {
+	if (position[el] <= 2023 && document.documentElement.clientWidth > 1300) {
+		position[el] += 289; 
+		carusel[el].style.right = position[el] + 'px';
 	}
 
-	if (position <= 2312 && document.documentElement.clientWidth > 1050 && document.documentElement.clientWidth <= 1300) {
-		position += 289; 
-		carusel.style.right = position + 'px';
+	if (position[el] <= 2312 && document.documentElement.clientWidth > 1050 && document.documentElement.clientWidth <= 1300) {
+		position[el] += 289; 
+		carusel[el].style.right = position[el] + 'px';
 	}
 
-	if (position <= 2601 && document.documentElement.clientWidth <= 1050 && document.documentElement.clientWidth > 750) {
-		position += 289; 
-		carusel.style.right = position + 'px';
+	if (position[el] <= 2601 && document.documentElement.clientWidth <= 1050 && document.documentElement.clientWidth > 750) {
+		position[el] += 289; 
+		carusel[el].style.right = position[el] + 'px';
 	}
 
-	if (position <= 2890 && document.documentElement.clientWidth <= 750) {
-		position += 289; 
-		carusel.style.right = position + 'px';
-	}
-}
-
-function load() {
-	position = 289;
-	carusel.style.right = position + 'px';
-	position = 0;
-	carusel.style.right = position + 'px';
-}
-
-function prev() {
-	if (position > 0) {
-		position -= 289; 
-		carusel.style.right = position + 'px';
+	if (position[el] <= 2890 && document.documentElement.clientWidth <= 750) {
+		position[el] += 289; 
+		carusel[el].style.right = position[el] + 'px';
 	}
 }
 
-prevbtn.addEventListener("click", prev);
-nextbtn.addEventListener("click", next)
+function load(el) {
+	position[el] = 0;
+	carusel[el].style.right = position[el] + 'px';
+}
 
-detectswipe('swipe', myfunction);
+function prev(el) {
+	if (position[el] > 0) {
+		position[el] -= 289; 
+		carusel[el].style.right = position[el] + 'px';
+	}
+}
+
+
+prevbtn[0].addEventListener("click", function() {prev(0)});
+nextbtn[0].addEventListener("click", function() {next(0)});
+
+prevbtn[1].addEventListener("click", function() {prev(1)});
+nextbtn[1].addEventListener("click", function() {next(1)});
+
+prevbtn[2].addEventListener("click", function() {prev(2)});
+nextbtn[2].addEventListener("click", function() {next(2)});
+
+detectswipe('swipe-popular', myfunction);
+detectswipe('swipe-news', myfunction);
+detectswipe('swipe-discount', myfunction);
